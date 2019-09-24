f58acb236cdc611e86804f95459aec99f83e152d
Asmita Andhale <asmita.andhale@flexur.com>
2019-09-24 17:15:17 +0530

    Project upload on Git

---
+++ MavenProject1/src/main/java/tests/NewTest.java
@@ -0,0 +1,29 @@
+package tests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+public class NewTest
+{
+	public WebDriver driver;
+	@Test
+	public void openMyBlog()
+	{
+		driver.get("https://www.softwaretestingmaterial.com/");
+	}
+	@BeforeClass
+	public void beforeClass()
+	{
+		String path = System.getProperty("user.dir");
+		System.out.println(path);
+		System.setProperty("webdriver.chrome.driver",path+"\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		System.out.println("Project upload on git");
+	}
+	@AfterClass
+	public void afterClass()
+	{
+		driver.quit();
+	}
+}
